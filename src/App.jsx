b328f7f49d97b6a53e479493370db2c6f376c538
Geeta <Geetatondareo8@gmail.com>
2025-11-17 12:35:54 +0530

    Added portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import CustomCursor from "./components/CustomCursor"
+import Navbar from "./components/Navbar"
+import About from "./sections/About"
+import Contact from "./sections/Contact"
+import Experience from "./sections/Experience"
+import Footer from "./sections/Footer"
+import Home from "./sections/Home"
+import Projects from "./sections/Projects"
+import Skills from "./sections/Skills"
+import Testimonials from "./sections/Testimonials"
+
+function App() {
+
+  return (
+    <div className="relative gradient text-white">
+      <CustomCursor/>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
